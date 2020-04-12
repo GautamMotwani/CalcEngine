e5cae3d90eab2e0c031a63a46d6e6a21d0291c89
Gautam Motwani <gautammmotwani@live.in>
2020-04-12 18:40:39 +0530

    Initial commit

---
+++ src/com/symbiantech/CalcEngine/MathEquation.java
@@ -0,0 +1,42 @@
+package com.symbiantech.CalcEngine;
+
+import java.security.PublicKey;
+
+public class MathEquation {
+    private double leftVals;
+    private double rightVals;
+    private char opCodes;
+    private double results;
+
+    public double getLeftVals () { return leftVals;}
+    public void setLeftVals (double leftVals) {this.leftVals = leftVals;}
+
+    public double getRightVals() {return rightVals;}
+    public void setRightVals(double rightVals) {this.rightVals = rightVals;}
+
+    public char getOpCodes() {return opCodes;}
+    public void setOpCodes(char opCodes) {this.opCodes = opCodes;}
+
+    public double getResults() {return results;}
+
+    public void execute() {
+    switch (opCodes) {
+        case 'a':
+            results = leftVals + rightVals;
+            break;
+        case 's':
+            results = leftVals - rightVals;
+            break;
+        case 'd':
+            results = rightVals != 0.0d ? leftVals / rightVals : 0.0d;
+            break;
+        case 'm':
+            results = leftVals * rightVals;
+            break;
+        default:
+            System.out.println("Incorrect opcodes entered");
+    }
+}
+
+
+}
